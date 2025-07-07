5714876e077ce21bea5efc1e6445377097a9609a
joaov-tst <joaovitortst777@gmail.com>
2025-07-07 19:15:23 -0300

    initial commit

---
+++ src/main/java/com/challenge/resilient_payment_router/ResilientPaymentRouterApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.resilient_payment_router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResilientPaymentRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResilientPaymentRouterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/resilient_payment_router/ResilientPaymentRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.resilient_payment_router;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResilientPaymentRouterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
