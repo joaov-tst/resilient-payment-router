e1a3ef0a353ce9d86847fa21de88f2e0eb159665
joaov-tst <joaovitortst777@gmail.com>
2025-07-16 20:21:41 -0300

    feat: habilita integracoes e scheduling

--- src/main/java/com/challenge/resilient_payment_router/ResilientPaymentRouterApplication.java
+++ src/main/java/com/challenge/resilient_payment_router/ResilientPaymentRouterApplication.java
@@ -2,8 +2,12 @@ package com.challenge.resilient_payment_router;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.challenge.resilient_payment_router.processors")
+@EnableScheduling
 public class ResilientPaymentRouterApplication {
 
 	public static void main(String[] args) {
